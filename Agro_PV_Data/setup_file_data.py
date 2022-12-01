31d1899cbcc6def58130b3919384dfef9f2123bd
siddharthy <siddharthyashaswee@gmail.com>
2022-12-02 00:54:56 +0100

    operational plants in east germany

---
+++ Agro_PV_Data/setup_file_data.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', 700)
+pd.set_option('display.max_columns', 100)
+
+
+# read the data
+# rename cols to english
+# convert lat and long values to float64
+def setup_data():
+    # set file path as relative path(if doesn't work then use raw string)
+    path = "./data/Stromerzeuger_east_data.csv"
+    path = r'D:\Main\RWTH Study\Data Analytics and Visualization Lab\pycharm_projects\DVA-LAB\Agro_PV_Data\data\Stromerzeuger_east_data.csv'
+    df = pd.read_csv(path, delimiter=";")
+
+    # removing columns where everything is NA
+    df.dropna(axis=1, how='all', inplace=True)
+
+    df = df.rename(columns={'MaStR-Nr. der Einheit': 'mastr_no_the_unit'})
+    df = df.rename(columns={'Anzeige-Name der Einheit': 'unit_friendly_name'})
+    df = df.rename(columns={'Betriebs-Status': 'operational_status'})
+    df = df.rename(columns={'Energieträger': 'energy_sources'})
+    df = df.rename(columns={'Bruttoleistung der Einheit': 'gross_power_of_the_unit'})
+    df = df.rename(columns={'Nettonennleistung der Einheit': 'net_power_rating_of_the_unit'})
+    df = df.rename(columns={'Inbetriebnahmedatum der Einheit': 'commissioning_date_of_the_unit'})
+    df = df.rename(columns={'Registrierungsdatum der Einheit': 'registration_date_of_the_unit'})
+    df = df.rename(columns={'Bundesland': 'federal_state'})
+    df = df.rename(columns={'Postleitzahl': 'postal_code'})
+    df = df.rename(columns={'Ort': 'location'})
+    df = df.rename(columns={'Straße': 'street'})
+    df = df.rename(columns={'Hausnummer': 'house_number'})
+    df = df.rename(columns={'Gemarkung': 'district'})
+    df = df.rename(columns={'Flurstück': 'parcel'})
+    df = df.rename(columns={'Gemeindeschlüssel': 'community_key'})
+    df = df.rename(columns={'Koordinate: Breitengrad (WGS84)': 'latitude'})
+    df = df.rename(columns={'Koordinate: Längengrad (WGS84)': 'longitude'})
+    df = df.rename(columns={'Anzahl der Solar-Module': 'number_of_solar_modules'})
+    df = df.rename(columns={'Hauptausrichtung der Solar-Module': 'main_orientation_of_the_solar_modules'})
+    df = df.rename(columns={'Lage der Einheit': 'location_of_the_unit'})
+    df = df.rename(columns={'Letzte Aktualisierung': 'last_update'})
+    df = df.rename(columns={'Datum der endgültigen Stilllegung': 'final_decommissioning_date'})
+    df = df.rename(columns={'Datum der geplanten Inbetriebnahme': 'date_of_planned_commissioning'})
+    df = df.rename(columns={'Name des Anlagenbetreibers (nur Org.)': 'name_of_the_plant_operator'})
+    df = df.rename(columns={r'MaStR-Nr. des Anlagenbetreibers': 'mastr_no_of_the_plant_operator'})
+    df = df.rename(columns={'Volleinspeisung oder Teileinspeisung': 'full_feed_or_partial_feed'})
+    df = df.rename(columns={'MaStR-Nr. der Genehmigung': 'mastr_no_of_approval'})
+    df = df.rename(columns={'Name des Anschluss-Netzbetreibers': 'name_of_the_connection_network_operator'})
+    df = df.rename(columns={'MaStR-Nr. des Anschluss-Netzbetreibers': 'mastr_no_of_the_connection_network_operator'})
+    df = df.rename(columns={'Netzbetreiberprüfung': 'network_operator_check'})
+    df = df.rename(columns={'Spannungsebene': 'voltage_level'})
+    df = df.rename(columns={'MaStR-Nr. der Lokation': 'mastr_no_the_location'})
+    df = df.rename(columns={'MaStR-Nr. der EEG-Anlage': 'mastr_no_the_eeg_system'})
+    df = df.rename(columns={'EEG-Anlagenschlüssel': 'eeg_system_key'})
+    df = df.rename(columns={'Inbetriebnahmedatum der EEG-Anlage': 'commissioning_date_of_the_eeg_system'})
+    df = df.rename(columns={'Installierte Leistung': 'installed_capacity'})
+    df = df.rename(columns={'Zuschlagnummer (EEG/KWK-Ausschreibung)': 'surcharge_number'})
+
+    # convert your lat and long to floats
+    df['latitude'] = df['latitude'].astype("string")
+    df['longitude'] = df['longitude'].astype("string")
+    df['latitude'] = df['latitude'].str.replace(',', '.')
+    df['longitude'] = df['longitude'].str.replace(',', '.')
+    df['latitude'] = df['latitude'].astype("float64")
+    df['longitude'] = df['longitude'].astype("float64")
+
+    return df
---
+++ Agro_PV_Data/vOperational_east.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import geopandas as gpd
+import contextily as cx
+from setup_file_data import setup_data
+
+pd.set_option('display.max_rows', 700)
+pd.set_option('display.max_columns', 100)
+
+#visualize all plants that are operational in east germany
+df = setup_data()
+df
+# find out all categories of operational status
+df['operational_status'].unique()
+# reults translation:
+# 'In operation', 'In planning', 'Finally decommissioned','Temporarily suspended'
+
+# selecting only operational PV
+opdf = df[df['operational_status'] == 'In Betrieb']
+# filter out latitudes and longitudes which are not NaN
+latdf = opdf[opdf['latitude'].notnull()]
+latdf = opdf[opdf['longitude'].notnull()]
+#check once
+latdf.isnull().sum()
+
+# make your own custom geopandas data frame using these GPS coordinates
+visdf = gpd.GeoDataFrame(latdf, geometry=gpd.points_from_xy(latdf.longitude, latdf.latitude, crs="epsg:4326"))
+# check the crs which is now GPS based
+visdf.crs
+# if you want to overlay these points onto the actual map of Germany
+# then first convert the coordinate reference system(crs) from GPS to epsg=3857
+gdf = visdf.to_crs(epsg=3857)
+# plot only the points first
+ax = gdf.plot()
+# then overlay the map using the epsg=3857 crs
+cx.add_basemap(ax)
